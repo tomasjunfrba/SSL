801b8477cc926b5fd566ca8efedf1d81dc6349ba
Tomas A. Jun <82248965+tomasjunfrba@users.noreply.github.com>
2024-10-20 18:48:37 -0300

    Create scanner.c

---
+++ TP Scanner/scanner.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include "scanner.h"
+#include "tablaDeTransiciones.h"
+#include <errno.h>
+
+#define TAMANIO_BUFFER 20
+
+char _buffer[TAMANIO_BUFFER+1];
+int  _pos = 0;
+
+void InicializarBuffer(void)
+{
+    memset(_buffer, 0, TAMANIO_BUFFER);
+}
+
+void LimpiarBuffer(void) {
+    memset(_buffer, 0, _pos + 1);
+    _pos = 0;
+}
+
+
+void AgregarCaracter(int caracter) {
+    _buffer[_pos++] = caracter;
+}
+
+const char *Buffer(void) {
+    _buffer[_pos] = '\0';
+    return _buffer;
+}
+
+TOKEN EsReservada(void) {
+    for(int i=0; i < CANTIDAD_PALABRAS_RESERVADAS; i++){
+        if(!strcmp(_buffer, palabras_reservadas[i])){
+            return RESERVADA_TOKEN;
+        }
+    }
+    return ID_TOKEN;
+}
+
+int EsReservadaV2(void) {
+    for (int i = 0; i < CANTIDAD_PALABRAS_RESERVADAS; i++) {
+        if (strcmp(_buffer, palabras_reservadas[i]) == 0) {
+            return 1;  // Es Reservada
+        }
+    }
+    return 0;  // No es Reservada
+}
+
+SIMBOLO ObtenerColumna(int simbolo) {
+    if (isalpha(simbolo)) {
+        return LETRA_SIMBOLO;
+    } else if (isdigit(simbolo)) {
+        return DIGITO_SIMBOLO;
+    } else if (simbolo == '+') {
+        return SUMA_SIMBOLO;
+    } else if (simbolo == '-') {
+        return RESTA_SIMBOLO;
+    } else if (simbolo == '(') {
+        return PARENTESIS_IZQUIERDO_SIMBOLO;
+    } else if (simbolo == ')') {
+        return PARENTESIS_DERECHO_SIMBOLO;
+    } else if (simbolo == ',') {
+        return COMA_SIMBOLO;
+    } else if (simbolo == ';') {
+        return PUNTO_Y_COMA_SIMBOLO;
+    } else if (simbolo == ':') {
+        return DOS_PUNTOS_SIMBOLO;
+    } else if (simbolo == '=') {
+        return IGUAL_SIMBOLO;
+    } else if (simbolo == -1) {
+        return FIN_DE_TEXTO_SIMBOLO;
+    } else if (isspace(simbolo)) {
+        return ESPACIO_SIMBOLO;
+    }    
+    return OTRO_SIMBOLO;
+}
+
+
+ESTADO Transicion(ESTADO estado, int simbolo) {
+    int columna = ObtenerColumna(simbolo);
+    return TablaDeTransiciones[estado][columna];
+}
+
+TOKEN Scanner(void) {
+    int c, pos = 0;
+    ESTADO estado = 0;
+    LimpiarBuffer();
+    while (c=getchar()) {
+        estado = Transicion(estado, c);
+        switch (estado) {
+            case 1:
+            case 3:
+                AgregarCaracter(c);
+                break;
+            case 2:
+                ungetc(c, stdin);
+                return EsReservada(); /* ID o RESERVADA*/
+            case 4:
+                ungetc(c, stdin);
+                return CONSTANTE_TOKEN; /* CONSTANTE */
+            case 5:
+                AgregarCaracter(c);
+                return SUMA_TOKEN; /* SUMA */
+            case 6:
+                AgregarCaracter(c);
+                return RESTA_TOKEN; /* RESTA */
+            case 7:
+                AgregarCaracter(c);
+                return PARENTESIS_IZQUIERDO_TOKEN;  /* PARENIZQUIERDO */
+            case 8:
+                AgregarCaracter(c);
+                return PARENTESIS_DERECHO_TOKEN;  /* PARENDERECHO */
+            case 9:
+                AgregarCaracter(c);
+                return COMA_TOKEN;  /* COMA */
+            case 10:
+                AgregarCaracter(c);
+                return PUNTO_Y_COMA_TOKEN;  /* PUNTOYCOMA */
+            case 11:
+                AgregarCaracter(c);
+                break;
+            case 12:
+                AgregarCaracter(c);
+                return ASIGNACION_TOKEN; /* ASIGNACION */
+            case 13:
+                return FIN_DE_TEXTO_TOKEN; /* FDT */
+            case -1:
+                errno = EINVAL; // EPERM operacion no permitida
+                perror("Error lexico"); /* ERROR */
+
+        }
+    }
+}
